4beda5bf2ce5e05b29afc42380b38577e314e4ec
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2023-02-08 21:55:20 -0500

    Fixes #31

---
+++ R/get-cur-comp-death-data.R
@@ -0,0 +1,85 @@
+#' Get Current Complications and Death Data.
+#'
+#' @family Hospital Data
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @seealso \url{https://data.cms.gov/provider-data/topics/hospitals/}
+#'
+#' @description Get the current complications and death data.
+#'
+#' @details This function will obtain the current Complications and Death data
+#' from the output of the [healthyR.data::current_hosp_data()] function, that is
+#' the required input for the `.data` parameter. You can pass in a list of which
+#' of those data sets you would like,
+#'
+#' @param .data The data that results from the `current_hosp_data()` function.
+#' @param .data_sets The default is: c("Facility","State","National"), which
+#' will bring back all of the data sets that are in the Ambulatory Surgery Center
+#' OAS CAHPS data sets. You can choose from the following:
+#' *  Facility
+#' *  National
+#' *  State
+#'
+#' You can also pass things like c("state","Nation") as behind the scenes only
+#' the Complicaitons and Death data sets are available to the function to choose
+#' from and `grep` is used to find matches with `ignore.case = TRUE` set.
+#'
+#' @examples
+#' \dontrun{
+#' library(dplyr)
+#'
+#' current_hosp_data() %>%
+#'   current_comp_death_data(.data_sets = c("State","National"))
+#' }
+#'
+#' @return
+#' Gets the current Complications and Death data from the current hospital data file.
+#'
+#' @name current_comp_death_data
+NULL
+
+#' @export
+#' @rdname current_comp_death_data
+
+current_comp_death_data <- function(.data,
+                                    .data_sets = c("Facility","State","National")) {
+
+    # Variables
+    ds <- .data_sets
+    l <- .data
+
+    # Checks
+    if (!inherits(l, "current_hosp_data")){
+        rlang::abort(
+            message = "'.data' must come from the function 'current_hosp_data()",
+            use_cli_format = TRUE
+        )
+    }
+
+    # Manipulations
+    # Get the exact files necessary to the ASC
+    file_names_vec <- c("Complications_and_Deaths_Hospital.csv",
+                        "Complications_and_Deaths_National.csv",
+                        "Complications_and_Deaths_State.csv")
+
+    asc_list <- l[names(l) %in% file_names_vec]
+    names(asc_list)[1] <-  "Complications_and_Deaths_Facility.csv"
+
+    # Make sure there are no 0 length items
+    asc_list <- asc_list[lengths(asc_list) > 0]
+
+    # Only keep the names we want
+    ret <- asc_list[grep(
+        paste(ds, collapse = "|"),
+        names(asc_list),
+        ignore.case = TRUE
+    )]
+
+    # Return\
+    attr(ret, ".list_type") <- "current_comp_death_list"
+    class(ret) <- c("current_comp_death_list", class(ret))
+
+    return(ret)
+
+}
